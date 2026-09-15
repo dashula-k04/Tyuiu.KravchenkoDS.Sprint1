f9032618744dd25e141d2b571f541a75d58282c4
skrav <skrav@LAPTOP-GUV9T24N>
2026-09-15 21:59:48 +0500

    Add project files.

---
+++ Tyuiu.KravchenkoDS.Sprint1.Task0.V15.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KravchenkoDS.Sprint1.Task0.V15.Lib;
+
+
+namespace Tyuiu.KravchenkoDS.Sprint1.Task0.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(24, res);
+        }
+    }
+}
